f2622eb64adb270ccba1dbe52a6a33ac436dfa7e
guirdy <guilherme.gl1997@gmail.com>
2023-03-28 14:49:27 -0300

    feat: theme and api request

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { GlobalStyle } from '@/styles/globals'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from '@/styles/themes/default'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Component {...pageProps} />
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,26 @@
+export const defaultTheme = {
+  colors: {
+    white: '#FFF',
+    background: '#000',
+
+    gray900: '#121214',
+    gray800: '#202024',
+    gray300: '#c4c4cc',
+    gray100: '#e1e1e6',
+
+    green800: '#006400',
+    green100: '#39FF14',
+
+    orange: '#F29901',
+    yellow: '#F8FE76',
+    blue: '#B2EAE4',
+  },
+
+  fontSizes: {
+    sm: '0.875rem',
+    md: '1.125rem',
+    lg: '1.25rem',
+    xl: '1.5rem',
+    '2xl': '2rem',
+  },
+}
